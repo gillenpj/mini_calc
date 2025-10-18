07c4c78da2fa01537013afdcef608e8c6a429738
Patrick Gillen <gillenpj@googlemail.com>
2025-10-18 19:09:05 +0100

    Fixed pytests

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,5 +1,17 @@
+from minicalc import add, sub, mul, div, powi
+
 def test_add():
 	assert add(2, 3) == 5
 
 def test_sub():
 	assert sub(5, 1) == 4
+
+def test_mul():
+	assert mul(5, 1) == 5
+
+def test_div():
+	assert div(5, 1) == 5
+
+def test_powi():
+	assert powi(5, 1) == 5
+
